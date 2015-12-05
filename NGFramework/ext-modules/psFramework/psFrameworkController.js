a5c29adfcde9a65cbd5373e22e2cedab1af05667
epdsn <ibestudios@gmail.com>
2015-12-04 17:12:31 -0700

    Set up left navigation and added functionality to a responsive menu.

--- NGFramework/ext-modules/psFramework/psFrameworkController.js
+++ NGFramework/ext-modules/psFramework/psFrameworkController.js
@@ -1,8 +1,55 @@
 ﻿"use strict";
 angular
     .module("psFramework").controller("psFrameworkController",
-        ['$scope',
-            function ($scope) {
+        ['$scope', '$rootScope', '$window', '$timeout',
+            function ($scope, $rootScope, $window, $timeout) {
+
+                $scope.isMenuVisible = true;
+                $scope.isMenuButtonVisible = true;
+
+                $scope.$on('ps-menu-item-selected-event', function (evt, data) {
+                    $scope.routeString = data.route;
+                    checkWidth();
+                    broadcastMenuState();
+                });
+
+                // attach event to window when it is resized
+                // the event is also given a namespace of .psFramework
+                $($window).on("resize.psFramework", function () {
+                    $scope.$apply(function () {
+                        checkWidth();
+                        broadcastMenuState();
+                    });
+                });
+                // when scope recieved the destroy, remove handler
+                $scope.$on("$destroy", function () {
+                    $($window).off("resize.psFramework");
+                });
+
+                var checkWidth = function () {
+                    // gets the full width of the window, even when the scrollbar shows up.
+                    var width = Math.max($($window).width(), $($window).innerWidth());
+                    $scope.isMenuVisible = (width > 768);
+                    $scope.isMenuButtonVisible = !$scope.isMenuVisible;
+                };
+
+                // handles when the responsive menu is clicked
+                $scope.menuButtonClicked = function () {
+                    $scope.isMenuVisible = !$scope.isMenuVisible;
+                    broadcastMenuState();
+                    //$scope.$apply();
+                };
+
+                var broadcastMenuState = function () {
+                    $rootScope.$broadcast('ps-menu-show',
+                        {
+                            show: $scope.isMenuVisible
+                        });
+                };
+
+                $timeout(function () {
+                    checkWidth();
+                }, 0);
 
             }
         ]);
--- NGFramework/ext-modules/psMenu/psMenuItemDirective.js
+++ NGFramework/ext-modules/psMenu/psMenuItemDirective.js
@@ -4,11 +4,26 @@ angular.module("psMenu").directive('psMenuItem', function () {
         require: "^psMenu",
         scope: {
             label: '@',
-            icon: '@'
+            icon: '@',
+            route: '@'
         },
         templateUrl: 'ext-modules/psMenu/psMenuItemTemplate.html',
         link: function (scope, el, attr, ctrl) {
+            el.on('click', function (evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
 
+                // check if item is active
+                scope.isActive = function () {
+                    return el === ctrl.getActiveElement();
+                };
+
+                scope.$apply(function () {
+                    // calls methods in controller psMenuController
+                    ctrl.setActiveElement(el);
+                    ctrl.setRoute(scope.route);
+                });
+            });
         }
 
     };
