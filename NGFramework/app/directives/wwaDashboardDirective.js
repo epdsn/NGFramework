252fb12d3c2e09b122d6cb1607c9d707c0d20fe3
epdsn <ibestudios@gmail.com>
2015-12-06 14:31:05 -0700

    Added routing. Added gridster scrpts for widgets.

---
+++ NGFramework/app/directives/wwaDashboardDirective.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+angular.module('app').directive('wwaDashboard', function () {
+    return {
+        restrict: 'AE',
+        scope: {},
+        template: '<h1>Dashboard Page</h1>',
+    };
+});
--- NGFramework/ext-modules/psDashboard/psDashboardModule.js
+++ NGFramework/ext-modules/psDashboard/psDashboardModule.js
@@ -1,4 +1,4 @@
 ﻿"use strict";
 
 angular
-    .module("psDashboard", []);
+    .module("psDashboard", ["gridster"]);
