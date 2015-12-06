e5ab17b1d2de65aae0f505b873a74fcb39e14496
epdsn <ibestudios@gmail.com>
2015-12-06 15:25:14 -0700

    Implimented gridster widgets.

--- NGFramework/app/directives/wwaDashboardDirective.js
+++ NGFramework/app/directives/wwaDashboardDirective.js
@@ -1,8 +1,47 @@
 ﻿"use strict";
 angular.module('app').directive('wwaDashboard', function () {
     return {
-        restrict: 'AE',
-        scope: {},
-        template: '<h1>Dashboard Page</h1>',
-    };
+        //restrict: 'AE',
+        
+        scope: {
+            // only used for attributes from the caller here.
+        },
+        // template must be snake case to work
+        template: '<ps-dashboard></ps-dashboard>',
+        link: function (scope) {
+
+            scope.title = 'Dashboard Title';
+
+            // for gridster options see:
+            // https://github.com/ManifestWebDesign/angular-gridster
+
+            scope.gridsterOpts = {
+                columns: 12,
+                margins: [20, 20],
+                outerMargin: false,
+                pushing: true,
+                floating: true,
+                swapping: false
+            };
+
+            scope.widgets = [
+            {
+                title: 'First',
+                // size of widget
+                sizeX: 3,
+                sizeY: 3,
+                // widget placement
+                row: 0,
+                col:0
+            },
+            {
+                title: 'Second',
+                sizeX: 2,
+                sizeY: 4,
+                row: 0,
+                col: 5
+            }
+            ];
+        }
+    }
 });
